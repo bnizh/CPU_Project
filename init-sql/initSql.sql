56b510b30df3977cca9cd3d0a3fd9c8decaa5586
nano <ntski14@freeuni.edu.ge>
2017-06-12 23:14:22 +0400

    changed scripts (msSql to mysql)

--- init-sql/initSql.sql
+++ init-sql/initSql.sql
@@ -1,36 +1,66 @@
-CREATE TABLE USERS (
-  id VARCHAR(11) NOT NULL PRIMARY KEY,
-  name NVARCHAR(255),
-  mobile VARCHAR(64),
-  birthDate DATE,
-  email VARCHAR(64),
-  password VARCHAR(255)
-)
+create schema if not exists CPU_DB default character set utf8;
+CREATE TABLE `USERS` (
+  `ID` VARCHAR(11) NOT NULL,
+  `name` VARCHAR(255) NULL ,
+  `mobile` VARCHAR(64) NULL,
+  `birthDate` DATE NULL,
+  `email` VARCHAR(64) NULL,
+  `password` VARCHAR(255) NULL,
+  PRIMARY KEY (`ID`));
 
-CREATE TABLE PERMISSION(
-  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  name NVARCHAR(64)
-)
-  
-CREATE TABLE USER_PERMISSION (
-  userId VARCHAR(11) NOT NULL,
-  permissionId INT NOT NULL,
-  UNIQUE (userID, permissionId),
-  FOREIGN KEY (userId) REFERENCES USERS(id),
-  FOREIGN KEY (permissionId) REFERENCES PERMISSION(id)
-)
 
-CREATE TABLE HOSPITAL (
-  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  name NVARCHAR(64),
-  address NVARCHAR(128),
-  phoneNumber NVARCHAR(64)
-)
+CREATE TABLE `PERMISSION` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(64) NULL,
+  PRIMARY KEY (`id`))
+COMMENT = 'permissions data table ';
 
-CREATE TABLE HOSPITAL_USERS(
-  userID VARCHAR(11) NOT NULL,
-  hospitalID INT NOT NULL,
-  UNIQUE (userID, hospitalID),
-  FOREIGN KEY (userID) REFERENCES USERS(id),
-  FOREIGN KEY (hospitalID) REFERENCES HOSPITAL(id)
-)
+CREATE TABLE `USER_PERMISSION` (
+  `userId` VARCHAR(11) NOT NULL,
+  `permissionId` INT NOT NULL,
+  INDEX `user_FK_idx` (`userId` ASC),
+  INDEX `permissions_FK_idx` (`permissionId` ASC),
+  CONSTRAINT `user_FK`
+    FOREIGN KEY (`userId`)
+    REFERENCES `cpu_db`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `permissions_FK`
+    FOREIGN KEY (`permissionId`)
+    REFERENCES `cpu_db`.`permission` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = 'table for permissions data for each user';
+
+
+ALTER TABLE USER_PERMISSION ADD UNIQUE unique_user_permission_couple (userId, permissionId);
+
+
+
+CREATE TABLE `cpu_db`.`HOSPITAL` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(64) NULL,
+  `address` VARCHAR(128) NULL,
+  `phoneNumber` VARCHAR(64) NULL,
+  PRIMARY KEY (`id`))
+COMMENT = 'hospitals table ';
+
+
+CREATE TABLE `HOSPITAL_USERS` (
+  `userId` VARCHAR(11) NOT NULL,
+  `hospitalId` INT NOT NULL,
+  INDEX `users_id_FK_idx` (`userId` ASC),
+  INDEX `hospital_id_FK_idx` (`hospitalId` ASC),
+  CONSTRAINT `users_id_FK`
+    FOREIGN KEY (`userId`)
+    REFERENCES `cpu_db`.`users` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `hospital_id_FK`
+    FOREIGN KEY (`hospitalId`)
+    REFERENCES `cpu_db`.`hospital` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = 'hospitals table ';
+
+ALTER TABLE HOSPITAL_USERS ADD UNIQUE  UNIQE_USER_HOSPITAL_COUPLE (userId, hospitalId);
