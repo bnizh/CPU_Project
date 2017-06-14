8dec5ac590483759508d77a63a78d4305e7fd406
andro <androkasrashvili@gmail.com>
2017-06-14 10:48:09 +0400

    Fix database utf8 encoding

--- src/server/database/manager/DbInfo.java
+++ src/server/database/manager/DbInfo.java
@@ -1,7 +1,7 @@
 package server.database.manager;
 
 public class DbInfo {
-	public static String DATABASE_INSTANCE = "jdbc:mysql://localhost:3306/CPU_DB";
+	public static String DATABASE_INSTANCE = "jdbc:mysql://localhost:3306/CPU_DB?useUnicode=true&characterEncoding=utf-8";
 	public static String DATABASE_USERNAME = "root";
 	public static String DATABASE_PASSWORD = "12200139";
 }
